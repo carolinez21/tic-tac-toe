661b92a7bc18b9e6c4e05bc9cffefed04c3448da
caroline <carzhang96@gmail.com>
2022-02-17 16:18:48 -0500

    organize into Game class and change to one Player class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,117 +1,89 @@
-# module that has a function which checks wins - make this module accessible to both
-module Win
-  def wins?
-    possible_win_cases = [[1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]]
-    winning_cases = possible_win_cases.select { |c| c.difference(self.allpositions).empty? }
-    winning_cases.any?
+WIN_CASES = [[1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]]
+
+class Game
+  attr_reader :positions, :current_player_index
+
+  def initialize
+    @positions = (1..9).to_a
+    @current_player_index = 0
+    @players = [Player.new(self, 'X'), Player.new(self, 'O')]
+  end
+
+  def print_board(array)
+    puts '     |     |     '
+    puts "  #{array[0]}  |  #{array[1]}  |  #{array[2]}  "
+    puts '_____|_____|_____'
+    puts '     |     |     '
+    puts "  #{array[3]}  |  #{array[4]}  |  #{array[5]}  "
+    puts '_____|_____|_____'
+    puts '     |     |     '
+    puts "  #{array[6]}  |  #{array[7]}  |  #{array[8]}  "
+    puts '     |     |     '
+  end
+
+  def wins?(player)
+    (WIN_CASES.select { |c| c.difference(player.allpositions).empty? }).any?
+  end
+
+  def game_over?(positions)
+    positions.none? { |p| (1..9).include?(p) }
+  end
+
+  def player_goes(player)
+    position = player.select_position
+    player.allpositions.push(position)
+    @positions[position - 1] = player.letter
+    print_board(@positions)
+  end
+
+  def current_player
+    @players[@current_player_index]
+  end
+
+  def other_player
+    @players[@current_player_index - 1]
+  end
+
+  def switch_players
+    @current_player_index = 1 - @current_player_index
+  end
+
+  def play
+    print_board(@positions)
+    loop do
+      player_goes(current_player)
+      if game_over?(positions)
+        puts 'Gosh darn, nobody won. How lame.'
+        return
+      elsif wins?(current_player)
+        puts "Yay!! #{current_player.letter} wins. Sucks to be you #{other_player.letter} :("
+        return
+      end
+      switch_players
+    end
   end
 end
 
-class PlayerX
-  extend Win
-  @@allpositions = []
-  @@letter = 'X'
+class Player
+  attr_reader :letter, :game
+  attr_accessor :allpositions
 
-  def self.allpositions
-    @@allpositions
+  def initialize(game, letter)
+    @letter = letter
+    @game = game
+    @allpositions = []
   end
 
-  def self.letter
-    @@letter
-  end
-
-  attr_reader :letter
-
-  def initialize(position)
-    @position = position
-    @@allpositions.push(position)
-  end
-
-  # keep track of all X positions
-end
-
-class PlayerO
-  extend Win
-  @@allpositions = []
-  @@letter = 'O'
-
-  def self.allpositions
-    @@allpositions
-  end
-
-  def self.letter
-    @@letter
-  end
-
-  attr_reader :letter
-
-  def initialize(position)
-    @position = position
-    @@allpositions.push(position)
-  end
-
-  # keep track of all O positions
-end
-
-# gameplay
-# draw game board
-# loop until there's a winner
-# puts PlayerX enter position
-# check if that position is taken
-# prints game board with x
-# check if there's a winner
-# puts Player O enter position
-# check if that position is taken
-# prints game board with O
-# check if there's a winner
-
-def print_board(array)
-  puts '     |     |     '
-  puts "  #{array[0]}  |  #{array[1]}  |  #{array[2]}  "
-  puts '_____|_____|_____'
-  puts '     |     |     '
-  puts "  #{array[3]}  |  #{array[4]}  |  #{array[5]}  "
-  puts '_____|_____|_____'
-  puts '     |     |     '
-  puts "  #{array[6]}  |  #{array[7]}  |  #{array[8]}  "
-  puts '     |     |     '
-end
-
-positions = (1..9).to_a
-print_board(positions)
-
-current_player = PlayerO
-other_player = PlayerX
-
-# check if game is over
-#if (positions.any? { |p| (1..9).include?(p) }).empty?
- # puts 'Gosh darn, nobody won. How lame.'
-#end
-def game_over?(positions)
-  positions.none? { |p| (1..9).include?(p) }
-end
-
-# loop
-until current_player.wins? || game_over?(positions)
-  temp = current_player
-  current_player = other_player
-  other_player = temp
-  puts "#{current_player}, choose where you'd like to go."
-  position = gets.chomp.to_i
-  # if that position is not valid
-  # if that position == X or O then print error message
-  # else, set to X
-  # while current_player.allpositions.include?(position) || other_player.allpositions.include?(position)
-  unless positions.include?(position)
-    puts 'Sorry, that is not a valid. Please choose another spot.'
+  def select_position
+    puts "Player #{@letter}, choose where you'd like to go:"
     position = gets.chomp.to_i
+    until @game.positions.include?(position)
+      puts 'Sorry, that position is not valid. Please choose another spot:'
+      position = gets.chomp.to_i
+      puts position
+    end
+    position
   end
-  current_player.new(position)
-  positions[position - 1] = current_player.letter
-  print_board(positions)
 end
-if game_over?(positions)
-  puts 'Gosh darn, nobody won. How lame.'
-else
-  puts "Yay!! #{current_player} wins. Sucks to be you #{other_player}."
-end
+
+Game.new.play
